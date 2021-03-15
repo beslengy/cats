2b94b1f355f3449504a931e521920b249778dd43
Yaroslav <tsar.leonid1@gmail.com>
2021-03-15 14:58:57 +0300

    added: - nav_graph - home fragment - data binding - safe args classpath, plugin and dependencies - navigation and fragment dependencies

---
+++ app/src/main/java/com/molchanov/cats/ui/home/HomeFragment.kt
@@ -0,0 +1,36 @@
+package com.molchanov.cats.ui.home
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import com.molchanov.cats.R
+import com.molchanov.cats.databinding.FragmentHomeBinding
+
+
+class HomeFragment : Fragment() {
+
+    private var _binding: FragmentHomeBinding? = null
+    private val binding get() = _binding!!
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        _binding = DataBindingUtil.inflate(inflater, R.layout.fragment_home, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
